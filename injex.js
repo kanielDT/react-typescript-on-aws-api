5be243eacb672d71c56788bd6e27ff515d9d931e
Kaniel D. Tapper <kanieldannn@gmail.com>
2023-05-04 16:01:57 -0400

    updated dog array

--- injex.js
+++ injex.js
@@ -6,6 +6,31 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+const dogs = [
+    {
+        name: "paco",
+        breed: "chiuahwah",
+        age: 32
+    },
+    {
+        name: "bob",
+        breed: "yorky",
+        age: 31
+    },
+    {
+        name: "juan",
+        breed: "pitbull",
+        age: 38
+    },
+    {
+        name: "Sabo",
+        breed: "husky",
+        age: 36
+    }
+
+]
+
 app.get("/", (req, res) => res.send("welcome to my API"))
 
 app.listen(4000, () => console.log("API running on port 4000"))
